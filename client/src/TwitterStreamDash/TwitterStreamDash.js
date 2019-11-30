e07afe5eb0b6cbded2a95ffda2e6fa3a0ffd9acb
Gauri <gauritech24@gmail.com>
2019-11-30 06:17:37 +0530

    fronend and backend connected, testing api created

--- client/src/TwitterStreamDash/TwitterStreamDash.js
+++ client/src/TwitterStreamDash/TwitterStreamDash.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ListView from './ListView/ListView'
-
+import axios from "axios";
 
 export default class TwitterStreamDash extends Component {
 
@@ -26,8 +26,16 @@ export default class TwitterStreamDash extends Component {
                 desc: "CNN purchased Casey Neistat's Beme app for $25million.",
                 image: "https://source.unsplash.com/user/erondu/600x400"
               },
-        ]
+        ];
 
+        axios.get('http://localhost:4000/abc')
+        .then(function (response) {
+          console.log(response.data);
+        })
+       .catch(function (error) {
+          console.log(error);
+       });
+        
        const card = PostsData.map((postcard, i) =>{
             return <ListView id={PostsData[i].id} img = {PostsData[i].image}title ={PostsData[i].title} desc ={PostsData[i].desc}/>
         }
--- index.js
+++ index.js
@@ -2,25 +2,36 @@ var express = require("express");
 
 let app = express();
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
-//twitter Api setup start
-var client = new Twitter({
-    consumer_key: "",
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
-  });
+// //twitter Api setup start
+// var client = new Twitter({
+//     consumer_key: "",
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     bearer_token: process.env.TWITTER_BEARER_TOKEN
+//   });
 
-//twitter Api setup end
+// //twitter Api setup end
 
 
 
 //ROUTE GET 
-app.get('/', (req, res) =>{
-    res.send('Home Route WOrking')
+app.get('/abc', (req, res) =>{
+    res.json({
+      message : "Happpy",
+      te:"AFjk"
+    })
 })
 
 // start local server at 3000 PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
 	console.log("server running");
 });
